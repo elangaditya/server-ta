f99de80d22315d79dd89c68d5d1ddc5a16f1e12a
Elang Aditya <elangadityar@gmail.com>
2022-03-16 20:53:45 +0700

    delete private

--- app/routes/private.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const validate = require('./tokenValidation');
-
-router.get('/', validate, (req, res) => {
-    res.send(req.user);
-});
-
-module.exports = router;
