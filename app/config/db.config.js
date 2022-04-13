b0147661c207df2d5f6cd97cf1113213c467a1cc
Elang Aditya <elangadityar@gmail.com>
2022-04-13 22:23:51 +0700

    dbtryagain

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    HOST: process.env.NODE_ENV !== 'production' ? 'localhost' : process.env.DB_HOST,
+    // HOST: process.env.NODE_ENV !== 'production' ? 'localhost' : process.env.DB_HOST,
     USER: process.env.DB_USER,
     PASSWORD: process.env.NODE_ENV !== 'production' ? '123456' : process.env.DB_PASSWORD,
     DB: process.env.NODE_ENV !== 'production' ? 'tugasakhir' : 'data_tugas_akhir',
