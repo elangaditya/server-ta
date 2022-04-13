bb1c95cbd6be7957c7c4f5337015e16ba5ad02a7
Elang Aditya <elangadityar@gmail.com>
2022-04-13 22:13:28 +0700

    dbtry

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     HOST: process.env.NODE_ENV !== 'production' ? 'localhost' : process.env.DB_HOST,
-    USER: 'ta-server',
+    USER: process.env.DB_USER,
     PASSWORD: process.env.NODE_ENV !== 'production' ? '123456' : process.env.DB_PASSWORD,
     DB: process.env.NODE_ENV !== 'production' ? 'tugasakhir' : 'data_tugas_akhir',
     dialect: 'mysql',
