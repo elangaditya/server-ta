ae97d8f405d236ddfd8e0f53c2870b2e41772c83
Elang Aditya <elangadityar@gmail.com>
2022-04-13 21:36:38 +0700

    testdblagi

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-    HOST: process.env.NODE_ENV !== 'production' ? 'localhost' : '/cloudsql/tugas-akhir-344812:asia-southeast2:database-tugas-akhir',
+    HOST: process.env.NODE_ENV !== 'production' ? 'localhost' : process.env.DB_HOST,
     USER: 'ta-server',
     PASSWORD: process.env.NODE_ENV !== 'production' ? '123456' : process.env.DB_PASSWORD,
     DB: process.env.NODE_ENV !== 'production' ? 'tugasakhir' : 'data_tugas_akhir',
     dialect: 'mysql',
+    dialectOptions: {
+        socketPath: process.env.DB_HOST,
+    },
     pool: {
         max: 5,
         min: 0,
