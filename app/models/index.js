78347fe19165f0fca07719f66c86e4dfd913459b
Elang Aditya <elangadityar@gmail.com>
2022-04-14 12:55:28 +0700

    cobadbtrus

--- app/models/index.js
+++ app/models/index.js
@@ -4,11 +4,8 @@ const dbConfig = require('../config/db.config');
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
     host: dbConfig.HOST,
     dialect: dbConfig.dialect,
-    pool: {
-        max: dbConfig.pool.max,
-        min: dbConfig.pool.min,
-        acquire: dbConfig.pool.acquire,
-        idle: dbConfig.pool.idle,
+    dialectOptions: {
+        socketPath: process.env.DB_HOST,
     },
 });
 
