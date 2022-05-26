90c4ec14afb65635b44722c2402061156ab5156d
Elang Aditya <elangadityar@gmail.com>
2022-05-26 15:15:18 +0700

    change db name

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,7 +2,7 @@ module.exports = {
     HOST: process.env.NODE_ENV !== 'production' ? 'localhost' : process.env.DB_HOST,
     USER: process.env.DB_USER,
     PASSWORD: process.env.NODE_ENV !== 'production' ? '123456' : process.env.DB_PASSWORD,
-    DB: process.env.NODE_ENV !== 'production' ? 'tugasakhir' : 'data_tugas_akhir',
+    DB: process.env.NODE_ENV !== 'production' ? 'tugasakhir' : 'tugas_akhir',
     dialect: 'mysql',
     dialectOptions: {
         socketPath: process.env.DB_HOST,
