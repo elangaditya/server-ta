985679dc611c533346e366be9f58164f15df7344
Elang Aditya <elangadityar@gmail.com>
2022-05-26 14:58:18 +0700

    old commit try

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,17 +1,11 @@
 module.exports = {
-    HOST: process.env.DB_HOST,
-    USER: process.env.DB_USERNAME,
-    PASSWORD: process.env.DB_PASSWORD,
-    DB: 'tugas_akhir',
+    HOST: process.env.NODE_ENV !== 'production' ? 'localhost' : process.env.DB_HOST,
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.NODE_ENV !== 'production' ? '123456' : process.env.DB_PASSWORD,
+    DB: process.env.NODE_ENV !== 'production' ? 'tugasakhir' : 'data_tugas_akhir',
     dialect: 'mysql',
     dialectOptions: {
         socketPath: process.env.DB_HOST,
-    },
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000,
     },
 };
 
--- app/models/index.js
+++ app/models/index.js
@@ -1,27 +1,35 @@
 const Sequelize = require('sequelize');
 const dbConfig = require('../config/db.config');
 
-const options = {
-    host: dbConfig.HOST,
-    dialect: dbConfig.dialect,
-    dialectOptions: {
-        socketPath: dbConfig.HOST,
-    },
-};
-
-// const devOptions = {
+// const options = {
 //     host: dbConfig.HOST,
 //     dialect: dbConfig.dialect,
-//     operatorsAliases: false,
-//     pool: {
-//         max: dbConfig.pool.max,
-//         min: dbConfig.pool.min,
-//         acquire: dbConfig.pool.acquire,
-//         idle: dbConfig.pool.idle,
+//     dialectOptions: {
+//         socketPath: dbConfig.HOST,
 //     },
 // };
 
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, options);
+// // const devOptions = {
+// //     host: dbConfig.HOST,
+// //     dialect: dbConfig.dialect,
+// //     operatorsAliases: false,
+// //     pool: {
+// //         max: dbConfig.pool.max,
+// //         min: dbConfig.pool.min,
+// //         acquire: dbConfig.pool.acquire,
+// //         idle: dbConfig.pool.idle,
+// //     },
+// // };
+
+// const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, options);
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+    dialectOptions: {
+        socketPath: process.env.DB_HOST,
+    },
+});
+
 
 const db = {};
 
