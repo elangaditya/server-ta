9e09395bb240ac57ad68d2c44a067234888d141e
Elang Aditya <elangadityar@gmail.com>
2022-05-26 14:03:22 +0700

    chg db name

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,7 +2,7 @@ module.exports = {
     HOST: process.env.NODE_ENV !== 'production' ? 'localhost' : process.env.DB_HOST,
     USER: process.env.NODE_ENV !== 'production' ? 'root' : process.env.DB_USERNAME,
     PASSWORD: process.env.NODE_ENV !== 'production' ? '123456' : process.env.DB_PASSWORD,
-    DB: process.env.NODE_ENV !== 'production' ? 'tugasakhir' : 'data_tugas_akhir',
+    DB: process.env.NODE_ENV !== 'production' ? 'tugasakhir' : 'tugas_akhir',
     dialect: 'mysql',
     dialectOptions: {
         socketPath: process.env.DB_HOST,
