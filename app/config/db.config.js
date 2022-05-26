7f53c8111c922cb2969a349807ab3d1bfa3f86cd
Elang Aditya <elangadityar@gmail.com>
2022-05-26 14:19:39 +0700

    change db config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: process.env.NODE_ENV !== 'production' ? 'localhost' : process.env.DB_HOST,
-    USER: process.env.NODE_ENV !== 'production' ? 'root' : process.env.DB_USERNAME,
-    PASSWORD: process.env.NODE_ENV !== 'production' ? '123456' : process.env.DB_PASSWORD,
-    DB: process.env.NODE_ENV !== 'production' ? 'tugasakhir' : 'tugas_akhir',
+    HOST: process.env.DB_HOST,
+    USER: process.env.DB_USERNAME,
+    PASSWORD: process.env.DB_PASSWORD,
+    DB: 'tugas_akhir',
     dialect: 'mysql',
     dialectOptions: {
         socketPath: process.env.DB_HOST,
@@ -14,3 +14,20 @@ module.exports = {
         idle: 10000,
     },
 };
+
+// module.exports = {
+//     HOST: process.env.NODE_ENV !== 'production' ? 'localhost' : process.env.DB_HOST,
+//     USER: process.env.NODE_ENV !== 'production' ? 'root' : process.env.DB_USERNAME,
+//     PASSWORD: process.env.NODE_ENV !== 'production' ? '123456' : process.env.DB_PASSWORD,
+//     DB: process.env.NODE_ENV !== 'production' ? 'tugasakhir' : 'tugas_akhir',
+//     dialect: 'mysql',
+//     dialectOptions: {
+//         socketPath: process.env.DB_HOST,
+//     },
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000,
+//     },
+// };
