f656e6793da64b724c4f154015ff1d1fde1b14a3
Elang Aditya <elangadityar@gmail.com>
2022-06-01 22:44:51 +0700

    police auth

--- app/routes/policeAuth.js
+++ app/routes/policeAuth.js
@@ -9,62 +9,62 @@ const { registerValidation, loginValidation } = require('./validation');
 
 // REGISTER ROUTE
 router.get('/register', (req, res) => {
-    res.render('pages/register');
+  res.render('pages/registerpolice');
 });
 
 router.post('/register', async (req, res) => {
-    // Validation
-    const { error } = registerValidation(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+  // Validation
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-    // Pre-exist check
-    const emailExist = await Police.findOne({ where: { email: req.body.email } });
-    if (emailExist) return res.status(409).send('Email already exist');
+  // Pre-exist check
+  const emailExist = await Police.findOne({ where: { email: req.body.email } });
+  if (emailExist) return res.status(409).send('Email already exist');
 
-    // Hashing
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  // Hashing
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-    // Save
-    const police = await Police.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: hashedPassword,
-    });
+  // Save
+  const police = await Police.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: hashedPassword,
+  });
 
-    try {
-        await police.save();
-        res.redirect('/auth/login');
-    } catch (err) {
-        res.status(400).send(err);
-    }
+  try {
+    await police.save();
+    res.redirect('/auth/login');
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
 
 // LOGIN ROUTE
 router.get('/login', (req, res) => {
-    res.render('pages/login');
+  res.render('pages/loginpolice');
 });
 
 router.post('/login', async (req, res) => {
-    // Validation
-    const { error } = loginValidation(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+  // Validation
+  const { error } = loginValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-    // Email Check
-    const police = await Police.findOne({ where: { email: req.body.email } });
-    if (!police) return res.status(400).send('Email doesnt exist');
+  // Email Check
+  const police = await Police.findOne({ where: { email: req.body.email } });
+  if (!police) return res.status(400).send('Email doesnt exist');
 
-    // Password Compare
-    const validPass = await bcrypt.compare(req.body.password, police.password);
-    if (!validPass) return res.status(400).send('Invalid password');
+  // Password Compare
+  const validPass = await bcrypt.compare(req.body.password, police.password);
+  if (!validPass) return res.status(400).send('Invalid password');
 
-    // Auth-token
-    const token = jwt.sign({ id: police.id, name: police.name }, process.env.POLICE_TOKEN_SECRET, {
-        expiresIn: '1h',
-    });
-    res.cookie('police_token', token, {
-        httpOnly: true, secure: false,
-    }).redirect('/api/home');
+  // Auth-token
+  const token = jwt.sign({ id: police.id, name: police.name }, process.env.POLICE_TOKEN_SECRET, {
+    expiresIn: '1h',
+  });
+  res.cookie('police_token', token, {
+    httpOnly: true, secure: false,
+  }).redirect('/police/api/home');
 });
 
 module.exports = router;
