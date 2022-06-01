d9cb41142c6b8720218cca55e4b386c8c2d74783
Elang Aditya <elangadityar@gmail.com>
2022-06-02 00:55:23 +0700

    disable db sync

--- public/index.js
+++ public/index.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-console */
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('../worker.js').then((registration) => {
-        console.log('SW Registered');
-        console.log(registration);
-    }).catch((error) => {
-        console.log('SW Registration Failed!');
-        console.log(error);
-    });
+  navigator.serviceWorker.register('../worker.js').then((registration) => {
+    console.log('SW Registered');
+    console.log(registration);
+  }).catch((error) => {
+    console.log('SW Registration Failed!');
+    console.log(error);
+  });
 }
