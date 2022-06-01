9ce827ad2b2ab92bf66adde8d126e3f14333ff54
Elang Aditya <elangadityar@gmail.com>
2022-06-02 00:50:27 +0700

    syncdb

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(express.urlencoded({ extended: true }));
 // eslint-disable-next-line no-unused-vars
 const db = require('./app/models');
 
-// db.sequelize.sync({ force: true });
+db.sequelize.sync({ force: true });
 
 // Public
 app.use(express.static('public'));
