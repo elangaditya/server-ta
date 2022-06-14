9e4a2aff8a2ff269d5e8c8254699677fe5eb677d
Elang Aditya <elangadityar@gmail.com>
2022-06-14 10:37:13 +0700

    extend token, add back

--- app/routes/notificationHandler.js
+++ app/routes/notificationHandler.js
@@ -1,21 +1,22 @@
-const router = require('express').Router();
-const webpush = require('web-push');
-const validate = require('./userValidation');
-const db = require('../models');
+const router = require("express").Router();
+const webpush = require("web-push");
+const validate = require("./userValidation");
+const db = require("../models");
 
 const Subscription = db.subscription;
 const User = db.user;
 
-const privateKey = 'O-fMSyBoXZRvoS391CW5c-0sSCNR22JwRPMBZKw3_3M';
-const publicKey = 'BG3LYBrcCZRQJyH9CfuVjteXWjCrCGBXDC9w8C0hQ9FwGBzcaBejAAcnsKwG-KCwdo5tz2kV62k4nh3B5hIgr0Q';
+const privateKey = "O-fMSyBoXZRvoS391CW5c-0sSCNR22JwRPMBZKw3_3M";
+const publicKey =
+  "BG3LYBrcCZRQJyH9CfuVjteXWjCrCGBXDC9w8C0hQ9FwGBzcaBejAAcnsKwG-KCwdo5tz2kV62k4nh3B5hIgr0Q";
 
-webpush.setVapidDetails('mailto:elang@google.com', publicKey, privateKey);
+webpush.setVapidDetails("mailto:elang@google.com", publicKey, privateKey);
 
-router.get('/subscribe', validate, (req, res) => {
-  res.render('pages/notif', { user: req.user });
+router.get("/subscribe", validate, (req, res) => {
+  res.render("pages/notif", { user: req.user });
 });
 
-router.post('/subscribe', validate, async (req, res) => {
+router.post("/subscribe", validate, async (req, res) => {
   // get push subscription object from the request
   // console.log(req.body);
   const { endpoint } = req.body;
@@ -28,10 +29,9 @@ router.post('/subscribe', validate, async (req, res) => {
   }).then(async (data) => {
     res.status(201).json({});
 
-    await User.findOne({ where: { id: req.user.id } })
-      .then((user) => {
-        data.setUser(user);
-      });
+    await User.findOne({ where: { id: req.user.id } }).then((user) => {
+      data.setUser(user);
+    });
 
     const subscription = {
       endpoint: data.endpoint,
@@ -42,36 +42,38 @@ router.post('/subscribe', validate, async (req, res) => {
     };
 
     // create paylod: specified the detals of the push notification
-    const payload = JSON.stringify({ title: 'Push Notification' });
+    const payload = JSON.stringify({ title: "Push Notification" });
     // console.log(subscription);
 
     // pass the object into sendNotification fucntion and catch any error
     // eslint-disable-next-line no-console
-    webpush.sendNotification(subscription, payload).catch((err) => console.error(err));
+    webpush
+      .sendNotification(subscription, payload)
+      .catch((err) => console.error(err));
   });
 });
 
-// router.post('/testpush', async (req) => {
-//     const subscription = await Subscription.findAll({
-//         where: { user_id: req.body.user_id },
-//     });
-//     subscription.forEach((data) => {
-//         const subData = {
-//             endpoint: data.endpoint,
-//             keys: {
-//                 p256dh: data.p256dh,
-//                 auth: data.auth,
-//             },
-//         };
+router.post("/testpush", async (req) => {
+  const subscription = await Subscription.findAll({
+    where: { user_id: req.body.user_id },
+  });
+  subscription.forEach((data) => {
+    const subData = {
+      endpoint: data.endpoint,
+      keys: {
+        p256dh: data.p256dh,
+        auth: data.auth,
+      },
+    };
 
-//         const message = {
-//             title: 'Pergerakan Tidak Terautorisasi Terdeteksi',
-//             body: `Terdeteksi pergerakan pada kendaraan Toyota Avanza.`,
-//         };
-//         webpush.sendNotification(subData, JSON.stringify(message)).catch((err) => {
-//             console.error(err);
-//         });
-//     });
-// });
+    const message = {
+      title: "Pergerakan Tidak Terautorisasi Terdeteksi",
+      body: `Terdeteksi pergerakan pada kendaraan Toyota Avanza.`,
+    };
+    webpush.sendNotification(subData, JSON.stringify(message)).catch((err) => {
+      console.error(err);
+    });
+  });
+});
 
 module.exports = router;
--- app/routes/userAuth.js
+++ app/routes/userAuth.js
@@ -1,26 +1,26 @@
-const router = require('express').Router();
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const db = require('../models');
-const { confirmationMail } = require('./mailHandler');
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const db = require("../models");
+const { confirmationMail } = require("./mailHandler");
 
 const User = db.user;
 
-const { registerValidation, loginValidation } = require('./validation');
+const { registerValidation, loginValidation } = require("./validation");
 
 // REGISTER ROUTE
-router.get('/register', (req, res) => {
-  res.render('pages/register');
+router.get("/register", (req, res) => {
+  res.render("pages/register");
 });
 
-router.post('/register', async (req, res) => {
+router.post("/register", async (req, res) => {
   // Validation
   const { error } = registerValidation(req.body);
   if (error) return req.flash(error.details[0].message);
 
   // Pre-exist check
   const emailExist = await User.findOne({ where: { email: req.body.email } });
-  if (emailExist) return req.flash('Email already registered');
+  if (emailExist) return req.flash("Email already registered");
 
   // Hashing
   const salt = await bcrypt.genSalt(10);
@@ -36,37 +36,44 @@ router.post('/register', async (req, res) => {
   try {
     const savedUser = await user.save();
     confirmationMail(savedUser);
-    res.redirect('/auth/login');
+    res.redirect("/auth/login");
   } catch (err) {
     res.status(400).send(err);
   }
 });
 
 // LOGIN ROUTE
-router.get('/login', (req, res) => {
-  res.render('pages/login');
+router.get("/login", (req, res) => {
+  res.render("pages/login");
 });
 
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   // Validation
   const { error } = loginValidation(req.body);
   if (error) return req.flash(error.details[0].message);
 
   // Email Check
   const user = await User.findOne({ where: { email: req.body.email } });
-  if (!user) return req.flash('Invalid email or password');
+  if (!user) return req.flash("Invalid email or password");
 
   // Password Compare
   const validPass = await bcrypt.compare(req.body.password, user.password);
-  if (!validPass) return req.flash('Invalid email or password');
+  if (!validPass) return req.flash("Invalid email or password");
 
   // Auth-token
-  const token = jwt.sign({ id: user.id, name: user.name }, process.env.TOKEN_SECRET, {
-    expiresIn: '2h',
-  });
-  res.cookie('auth_token', token, {
-    httpOnly: true, secure: false,
-  }).redirect('/api/dashboard');
+  const token = jwt.sign(
+    { id: user.id, name: user.name },
+    process.env.TOKEN_SECRET,
+    {
+      expiresIn: "6h",
+    }
+  );
+  res
+    .cookie("auth_token", token, {
+      httpOnly: true,
+      secure: false,
+    })
+    .redirect("/api/dashboard");
 });
 
 router.get("/logout", (req, res) => {
@@ -74,7 +81,7 @@ router.get("/logout", (req, res) => {
 });
 
 // EMAIL CONFIRMATION ROUTE
-router.get('/:token', async (req, res) => {
+router.get("/:token", async (req, res) => {
   const verified = jwt.verify(req.params.token, process.env.TOKEN_EMAIL);
   // res.send(verified);
   const user = await User.findByPk(verified.id);
